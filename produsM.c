14a0cd6a29bead9658fa4808dd15ee75a724d970
Otilia Biciila <otilia_teodora@yahoo.com>
2015-06-23 15:49:20 +0300

    modified source for matrix multiplication ^_^

--- produsM.c
+++ produsM.c
@@ -8,14 +8,17 @@ int main()
     int n1, m1, n2, m2;
     int i, j, k;
     
+    printf("Dimensiune matrice 1:\n");
     scanf("%d%d", &n1, &m1);
+    printf("Elemente matrice 1:\n");
     for(i = 0;i < n1;i ++)
         for(j = 0;j < m1;j ++) {
             scanf("%d", &a[i][j]);
             a[i][j] = 0;
             }
-    
+    printf("Dimensiune matrice 2:\n");
     scanf("%d%d", &n2, &m2);
+    printf("Elemente matrice 2:\n");
     for(i = 0;i < n2;i ++)
         for(j = 0;j < m2;j ++){
             scanf("%d", &b[i][j]);
@@ -27,13 +30,16 @@ int main()
         printf("matricele nu pot fi inmultite!");
         return 0;
     }
-    
+    int s;
     // inmultire matrice
     for(i = 0;i < n1;i ++)
         for(j = 0; j < m1;j ++)
+        {
+        	s = 0;
             for(k = 0; k < m2; k ++)
-                rez[i][j] += a[i][j] * b[j][k];
-                
+                s += a[i][k] * b[k][j];
+            rez[i][j] = s;
+        }      
     // afisare matrice rezultat
     printf("Matricea rezultat este: \n");
     for(i = 0;i < n1;i ++)
@@ -42,5 +48,5 @@ int main()
             printf("%d ", rez[i][j]);
         printf("\n");
     }
-   // hello world 
+   return 0;
 }
